db1fe81fee45cdd1b353e31e90859af35fab79c1
Developerryou <developerryou@gmail.com>
2023-09-26 23:41:08 +0900

    FEAT: passphrase 입력가능하도록 수정

--- src/pages/Popup/Api.tsx
+++ src/pages/Popup/Api.tsx
@@ -3,24 +3,38 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import apiGet from '@ipc/api/apiGet';
 import apiSave from '@ipc/api/apiSave';
+import channel from '@channel';
 
 const Api: React.FC = () => {
   const [accessKey, setAccessKey] = useState('');
   const [secretKey, setSecretKey] = useState('');
+  const [passphrase, setPassphrase] = useState('');
+  const [bankname, setBankname] = useState('');
 
   const onChangeInput = e => {
     const {value, name} = e.target;
     if (name === 'accessKey') setAccessKey(value);
     if (name === 'secretKey') setSecretKey(value);
+    if (name === 'passphrase') setPassphrase(value);
   };
 
   const onSubmit = e => {
     e.preventDefault();
-    apiSave({accessKey: accessKey, secretKey: secretKey});
+    apiSave({
+      accessKey: accessKey,
+      secretKey: secretKey,
+      passphrase: passphrase,
+    });
   };
 
   useEffect(() => {
-    apiGet(setAccessKey, setSecretKey);
+    apiGet(setAccessKey, setSecretKey, setPassphrase); // setBank
+
+    const {ipcRenderer} = window.require('electron');
+    ipcRenderer.on(channel.api.exchange.getName, (event, res) => {
+      setBankname(res);
+    });
+    ipcRenderer.send(channel.api.exchange.getName);
   }, []);
 
   return (
@@ -46,6 +60,17 @@ const Api: React.FC = () => {
               onChange={onChangeInput}
             ></input>
           </div>
+          {bankname === 'okx' && (
+            <div className="mb-3">
+              <Title className="fs-4 mb-5">PASSPHRASE</Title>
+              <input
+                name="passphrase"
+                placeholder="passphrase"
+                value={passphrase}
+                onChange={onChangeInput}
+              ></input>
+            </div>
+          )}
           <button className="w-100 btn-contained-blue" type="submit">
             <p> API KEYS 저장하기</p>
           </button>
