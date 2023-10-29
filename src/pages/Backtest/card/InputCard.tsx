33d6ed9d95aa6cc6ef9113067f097a91899c7df5
GyeongSik Son <47748085+Son-GyeongSik@users.noreply.github.com>
2023-10-30 00:36:27 +0900

    fix : fix start account typing bug

--- src/pages/Backtest/card/InputCard.tsx
+++ src/pages/Backtest/card/InputCard.tsx
@@ -32,7 +32,8 @@ const InputCard: React.FC = () => {
 
   const initError = {
     preset: '',
-    startBalance: '',
+    longStartBalance: '',
+    shortStartBalance: '',
     startDate: '',
     endDate: '',
   };
@@ -48,7 +49,7 @@ const InputCard: React.FC = () => {
 
   const onBlur = e => {
     const {value, name} = e.target;
-    if (name === 'startBalance') {
+    if (name === 'longStartBalance' || name === 'shortStartBalnce') {
       let newInput = {...input};
       if (isNaN(parseInt(value))) newInput[name] = 0;
       else newInput[name] = parseInt(value);
@@ -93,26 +94,26 @@ const InputCard: React.FC = () => {
           <div className="mb-4">
             <Label title="Long 시작잔고" hasTag>
               <input
-                name="startBalance"
+                name="longStartBalance"
                 placeholder="직접 입력"
                 value={input.longStartBalance}
                 onChange={onChangeInput}
                 onBlur={onBlur}
               ></input>
             </Label>
-            <Error className="mt-5" content={error.startBalance} />
+            <Error className="mt-5" content={error.longStartBalance} />
           </div>
           <div className="mb-4">
             <Label title="Short 시작잔고" hasTag>
               <input
-                name="startBalance"
+                name="shortStartBalance"
                 placeholder="직접 입력"
                 value={input.shortStartBalance}
                 onChange={onChangeInput}
                 onBlur={onBlur}
               ></input>
             </Label>
-            <Error className="mt-5" content={error.startBalance} />
+            <Error className="mt-5" content={error.shortStartBalance} />
           </div>
 
           <div className="mb-4">
